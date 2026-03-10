fc2d96855bb9b1448a0ad9d88b19cac9d03b8047
harii120 <u2103101@rajagiri.edu.in>
2026-03-11 03:38:56 +0530

    basic homepage and routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to my science blog!</h1>
-      <p>This is the home page; add some introductory content here.</p>
+      <h1 className=" text-white p-4">Welcome to my science blog!</h1>
+      <p className=" text-white p-4">This is the home page; add some introductory content here.</p>
     </main>
   );
 }
