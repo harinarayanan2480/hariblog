d657be571e51f4b39482d62543822994b29ab185
harii120 <u2103101@rajagiri.edu.in>
2026-03-11 01:41:11 +0530

    Boilerplate

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to my science blog!</h1>
+      <p>This is the home page; add some introductory content here.</p>
+    </main>
+  );
+}
---
+++ src/pages/Topics.jsx
@@ -0,0 +1,8 @@
+export default function Topics() {
+  return (
+    <main>
+      <h1>Topics</h1>
+      <p>List your blog topics here.</p>
+    </main>
+  );
+}
